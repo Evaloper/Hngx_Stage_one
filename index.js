e19dc3cac56a7add7dc7ad7b73bd5adfc886775c
Evaloper <gbajeeva@yahoo.com>
2023-09-09 16:58:42 +0100

    Time fixing

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const setTime = new Date();
-const hours = setTime.getUTCHours();
-const minutes = setTime.getUTCMinutes();
-const seconds = setTime.getUTCSeconds();
-const milliseconds = setTime.getUTCMilliseconds();
 
-const actualUTCTime = [`${hours}:${minutes}:${seconds}:${milliseconds}`]
-
-const currentTime = document.getElementById("currentTime");
-currentTime.innerHTML = actualUTCTime.toString();
-
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
 const day = weekday[setTime.getDay()];
 const currentDay = document.getElementById("currentDay");
 currentDay.innerHTML = day;
+
+function updateTime() {
+    const currentTime = document.getElementById("currentTime");
+
+    function updateUTCTime() {
+        // const actualUTCTime = { hour: 'time', minute: 'time', second: 'time' };
+        // const currentUTCTime = new Date().toLocaleTimeString(undefined, actualUTCTime);
+
+        const hours = setTime.getUTCHours();
+        const minutes = setTime.getUTCMinutes();
+        const seconds = setTime.getUTCSeconds();
+        const milliseconds = setTime.getTime();
+
+        const actualUTCTime = [`${hours}:${minutes}:${seconds}:${milliseconds}`];
+
+        currentTime.innerHTML = actualUTCTime;
+    }
+    updateUTCTime();
+    setInterval(updateUTCTime, 1);
+}
+
+updateTime();
